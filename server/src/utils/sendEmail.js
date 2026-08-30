258de4922ac07d291bbb25cb124542d3d8943630
snehamandal0001 <snehamandal858@gmail.com>
2026-08-31 02:11:35 +0530

    test email 1

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,12 +1,38 @@
+// import nodemailer from 'nodemailer';
+
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     service: 'Gmail',
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: `PlacementHub <${process.env.EMAIL_USER}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// export default sendEmail;
+
+
 import nodemailer from 'nodemailer';
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // Forces SSL 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
-    },
+    }
   });
 
   const mailOptions = {
