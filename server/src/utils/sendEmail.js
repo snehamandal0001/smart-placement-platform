a2ec0a0aa731d05c5a4c5e35512925d69ebdab5f
snehamandal0001 <snehamandal858@gmail.com>
2026-08-31 12:43:20 +0530

    Swapped Nodemailer for Resend API

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -22,32 +22,54 @@
 // export default sendEmail;
 
 
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-import dns from 'dns';
+// import dns from 'dns';
 
-// Force Node.js to use standard IPv4 addresses instead of IPv6
-dns.setDefaultResultOrder('ipv4first');
+// // Force Node.js to use standard IPv4 addresses instead of IPv6
+// dns.setDefaultResultOrder('ipv4first');
+
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 465,
+//     secure: true, // Forces SSL 
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     }
+//   });
+
+//   const mailOptions = {
+//     from: `PlacementHub <${process.env.EMAIL_USER}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// export default sendEmail;
+
+
+
+import { Resend } from 'resend';
+
+// Initialize Resend with your API Key
+//const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Forces SSL 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    }
-  });
 
-  const mailOptions = {
-    from: `PlacementHub <${process.env.EMAIL_USER}>`,
-    to: options.email,
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  await resend.emails.send({
+    // ON THE FREE TIER: The 'from' address MUST be onboarding@resend.dev
+    from: 'PlacementHub <onboarding@resend.dev>', 
+    to: options.email, 
     subject: options.subject,
     text: options.message,
-  };
-
-  await transporter.sendMail(mailOptions);
+  });
 };
 
 export default sendEmail;
